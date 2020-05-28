d4472371d3b34e087a9e1053f2141c75f64068f7
Kent C. Dodds <me+github@kentcdodds.com>
2020-05-28 12:44:35 -0600

    exercises/13-integration-testing

---
+++ src/__tests__/book-screen.extra-2.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {queryCache} from 'react-query'
+import * as auth from 'auth-provider'
+import {buildUser, buildBook} from 'test/generate'
+import * as usersDB from 'test/data/users'
+import * as booksDB from 'test/data/books'
+import {formatDate} from 'utils/misc'
+import {AppProviders} from 'context'
+import {App} from 'app'
+
+// general cleanup
+afterEach(async () => {
+  queryCache.clear()
+  await Promise.all([auth.logout(), usersDB.reset(), booksDB.reset()])
+})
+
+test('renders all the book information', async () => {
+  const user = buildUser()
+  await usersDB.create(user)
+  const authUser = usersDB.authenticate(user)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'Test page', route)
+
+  render(<App />, {wrapper: AppProviders})
+
+  await waitFor(() => {
+    if (queryCache.isFetching) {
+      throw new Error('The react-query queryCache is still fetching')
+    }
+    if (screen.queryByLabelText(/loading/i) || screen.queryByText(/loading/i)) {
+      throw new Error('App loading indicators are still running')
+    }
+  })
+
+  expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.publisher)).toBeInTheDocument()
+  expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+    'src',
+    book.coverImageUrl,
+  )
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('textbox', {name: /notes/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+  expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+})
+
+test('can create a list item for the book', async () => {
+  const user = buildUser()
+  await usersDB.create(user)
+  const authUser = usersDB.authenticate(user)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'Test page', route)
+
+  render(<App />, {wrapper: AppProviders})
+
+  await waitFor(() => {
+    if (queryCache.isFetching) {
+      throw new Error('The react-query queryCache is still fetching')
+    }
+    if (screen.queryByLabelText(/loading/i) || screen.queryByText(/loading/i)) {
+      throw new Error('App loading indicators are still running')
+    }
+  })
+
+  const addToListButton = screen.getByRole('button', {name: /add to list/i})
+  userEvent.click(addToListButton)
+  expect(addToListButton).toBeDisabled()
+
+  await waitFor(() => {
+    if (queryCache.isFetching) {
+      throw new Error('The react-query queryCache is still fetching')
+    }
+    if (screen.queryByLabelText(/loading/i) || screen.queryByText(/loading/i)) {
+      throw new Error('App loading indicators are still running')
+    }
+  })
+
+  expect(
+    screen.getByRole('button', {name: /mark as read/i}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('button', {name: /remove from list/i}),
+  ).toBeInTheDocument()
+  expect(screen.getByRole('textbox', {name: /notes/i})).toBeInTheDocument()
+
+  const startDateNode = screen.getByLabelText(/start date/i)
+  expect(startDateNode).toHaveTextContent(formatDate(Date.now()))
+
+  expect(
+    screen.queryByRole('button', {name: /add to list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+})
